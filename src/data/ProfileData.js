fb656c941db9d04430bd5bca7ac28c5667746e62
georgemiraclee <georgemiracle2019@gmail.com>
2023-11-08 14:19:58 +0700

    first commit

---
+++ src/data/ProfileData.js
@@ -0,0 +1,8 @@
+// data/ProfileData.js
+export const groupMembers = [
+    { id: 1, name: "Jericho Nathanael", nim: "21120121140160", profilePicture: "https://media-cgk1-2.cdn.whatsapp.net/v/t61.24694-24/131238688_408590480406008_1466034247561344838_n.jpg?ccb=11-4&oh=01_AdQCismqogrYI9aV_C38WMSR979uQNEodiFD0MSDKw6C4Q&oe=6554A734&_nc_sid=e6ed6c&_nc_cat=103" },
+    { id: 2, name: "Vane Karenina Thetan Hartono", nim: "21120121120005", profilePicture: "https://media-cgk1-2.cdn.whatsapp.net/v/t61.24694-24/349215516_3521002918178959_7650341622246269920_n.jpg?ccb=11-4&oh=01_AdQYo79fguUaFP88QIJSh9zcE8OfspzEaQxgmbNh1NK8_A&oe=6552D632&_nc_sid=e6ed6c&_nc_cat=101" },
+    { id: 3, name: "Bima Abizard Nurqolbi", nim: "21120121140107", profilePicture: "https://media-cgk1-2.cdn.whatsapp.net/v/t61.24694-24/300016065_648884006601929_6820823154457359587_n.jpg?ccb=11-4&oh=01_AdRXv0vAXQ1-lxVK-LP0NpsB5VTzv4eP-cdzmuvsMIZHLw&oe=6552E9AB&_nc_sid=e6ed6c&_nc_cat=102" },
+    { id: 4, name: "George Miracle Theophylus", nim: "21120121140031", profilePicture: "https://media-cgk1-2.cdn.whatsapp.net/v/t61.24694-24/348789515_778954883633208_4342475238609211439_n.jpg?ccb=11-4&oh=01_AdRWNARd6cC_EnBOAYB-8e80Cn8E5iS0xqsi9vDXLV6qEg&oe=6552F05D&_nc_sid=e6ed6c&_nc_cat=108" },
+  ];
+  
